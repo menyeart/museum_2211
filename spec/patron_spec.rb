33a6b93a2245749ad4af183a697cfc6b818381dc
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:14:58 -0700

    Add a third test for the spending money attribue

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Patron do
     expect(patron_1.name).to eq("Bob")
   end
 
+  it "has spending money" do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.spending_money).to eq(20)
+  end
+
 
 
 
