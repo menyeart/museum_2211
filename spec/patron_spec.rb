adef71333e98ada87a35ba2701a0c9d55f2e93dc
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:10:59 -0700

    Create a patron spec and the first test

---
+++ spec/patron_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Patron do
+  it "exists" do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1).to be_an_instance_of(Patron)
+  end
+
+
+
+end
