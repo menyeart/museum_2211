280a6cd1da0a09a1a154c23b8841d549370fc7d5
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:26:18 -0700

    Add a test for the name instance variable

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,8 +6,14 @@ require './lib/museum'
 RSpec.describe Museum do
   it "exists" do
     dmns = Museum.new("Denver Museum of Nature and Science")
-    expect(dms).to be_an_instance_of(Museum)
+    expect(dmns).to be_an_instance_of(Museum)
   end
+
+  it "has a name" do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    expect(dmns.name).to eq("Denver Museum of Nature and Science")
+  end
+
 
 
 
