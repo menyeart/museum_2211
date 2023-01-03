47988ad20b068b8a138664370c0adbeae571b8fd
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:06:54 -0700

    Add a test for name

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Exhibit do
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     expect(exhibit).to be_an_instance_of(Exhibit)
   end
+
+    it "has a name" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
   
 end
