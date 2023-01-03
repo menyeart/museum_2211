634f4a3f3a60b86040fcd65839abd0f9761b6900
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:04:23 -0700

    Create the first test

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,9 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
+  it "exists" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
   
 end
