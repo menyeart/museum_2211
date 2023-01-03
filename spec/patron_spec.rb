770387e668a7fbed2d8f92c010227d95191e4e79
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 09:17:00 -0700

    Add a 4th test for the interests instance variable

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -18,6 +18,12 @@ RSpec.describe Patron do
     expect(patron_1.spending_money).to eq(20)
   end
 
+  it "does not start with any interests" do
+    patron_1 = Patron.new("Bob", 20)
+    expect(patron_1.interests).to eq([])
+  end
+
+
 
 
 
